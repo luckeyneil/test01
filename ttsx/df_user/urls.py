3cea9ff95a058291c0463786fd89d96a74c74bdd
zy <neilasbefore@163.com>
2018-02-28 21:03:57 +0800

    反清空

--- ttsx/df_user/urls.py
+++ ttsx/df_user/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from df_user import views
 
 urlpatterns = [
-
+    url(r'^index3', views.index3),
+    url(r'^index2', views.index2),
+    url(r'^', views.index),
+    url(r'^1', views.index),
 ]
